bcc53ea24f125c7d884df7761cabd933abc8213b
Pavel Mikhaylov <pasmikh@gmail.com>
2025-01-09 18:11:08 +0200

    Fix audio service

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "937445262"
+    serviceWorkerVersion: "554523606"
   }
 });
